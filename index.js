1d3b85514329d1c13bd0bbd8d518bd28fa971d11
Daviicode <d.ydzdavid@yahoo.com>
2019-05-23 19:53:06 -0400

    Done.

--- index.js
+++ index.js
@@ -20,39 +20,38 @@ class Polygon {
     return sum;
   }
 
-  class Triangle extends Polygon {
-    get isValid () {
-      if ( this.count === 3) {
-        let sides1 = this.sides[0];
-        let sides2 = this.sides[1];
-        let sides3 = this.sides[2];
-        if ( sides1 + sides2 > sides3 && sides1 + sides3 > sides2 && sides2 + sides3 > sides1) {
-          return true;
-        } else { return false;}
-      }
-      else {
-        return false
-      }
-    }
-  }
 
-  class Square extends polygon {
-
-    get isValid() {
-      if (this.sides === 4) {
-        let sides1 = this.sides[0];
-        let sides2 = this.sides[1];
-        let sides3 = this.sides[2];
-        let sides4 = this.sides[3];
-        if (sides1 === sides2 && sides2 === sides3 && sides3 === sides4) {
-          return true;
-        } else {return false;}
+}
+class Triangle extends Polygon {
+  get isValid () {
+    if ( this.count === 3) {
+      let sides1 = this.sides[0];
+      let sides2 = this.sides[1];
+      let sides3 = this.sides[2];
+      if ( sides1 + sides2 > sides3 && sides1 + sides3 > sides2 && sides2 + sides3 > sides1) {
+        return true;
       } else { return false;}
     }
-
-    get area() {
-      return this.sides[0] ** 2;
+    else {
+      return false
     }
   }
+}
 
+class Square extends Polygon {
+  get isValid() {
+    if (this.sides === 4) {
+      let sides1 = this.sides[0];
+      let sides2 = this.sides[1];
+      let sides3 = this.sides[2];
+      let sides4 = this.sides[3];
+      if (sides1 === sides2 && sides2 === sides3 && sides3 === sides4) {
+        return true;
+      } else {return false;}
+    } else { return false;}
+  }
+
+  get area() {
+    return this.sides[0] ** 2;
+  }
 }
